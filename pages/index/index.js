3cae636ce1f53cb15ea5628d3739fe93f3cfc3f7
FredYang201 <fredyang1402143595@gmail.com>
2020-06-29 18:02:31 -0700

    v1

---
+++ pages/index/index.js
@@ -0,0 +1,67 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    weatherInfo: null,
+    city: 'san francisco'
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  btnClick: function() {
+    var thispage = this;
+    app.getExpressInfo(this.data.city, function(data) {
+      thispage.setData({
+        weatherInfo: data
+      })
+    })
+  },
+
+  input: function(e){
+    this.setData({
+      city: e.detail.value
+    })
+  }
+})
